bd709e6dc84b2a95a257ac1e52b35168082275f8
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-03 18:13:07 +0000

    wergtwergr

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateTheVolume() {
+
+  // input
+  const length = parseFloat(document.getElementById('length').value)
+  const width = parseFloat(document.getElementById('width').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+  // process
+  const volume = (length*width*height)/3
+
+
+  // output
+  document.getElementById('volume').innerHTML = 'volume is : ' + volume + 'mm²'
+}
